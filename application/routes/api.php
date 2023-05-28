a2297cd9f7a40f53f45ec13c895384762ce40792
Amany Saad <eng.am.aboelella@gmail.com>
2023-05-28 03:15:05 +0300

    using scantum for basic auth

--- application/routes/api.php
+++ application/routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::controller(\App\Http\Controllers\API\RegisterController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
