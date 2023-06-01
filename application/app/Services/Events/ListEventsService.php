78003e306e714fba4cb856de32c6780a8bbe79fc
Amany Saad <eng.am.aboelella@gmail.com>
2023-06-01 13:12:17 +0300

    add new column weather to events for storing weather data when storing new event to avoid performance issue when listing all events

---
+++ application/app/Services/Events/ListEventsService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services\Events;
+
+use App\Adapters\Weather\WeatherProviderInterface;
+use App\Resources\Events\ShowEventResource;
+use App\Traits\Response;
+use Illuminate\Support\Facades\Auth;
+
+class ListEventsService
+{
+    use Response;
+    protected WeatherProviderInterface $weather;
+
+    public function __construct( WeatherProviderInterface $weather){
+        $this->weather=$weather;
+    }
+
+
+    public function handle($event)
+    {
+        if($this->checkAuthorization($event)){
+            try {
+               $event['weather']=$this->weather->currentWeather($event->latitude,$event->longitude,strtotime($event->date_time));
+                return $this->success(new ShowEventResource($event))->updated();
+            } catch (\Exception $exception) {
+                return $this->error()->server();
+            }
+        }else{
+            return $this->error()->forbidden();
+        }
+
+    }
+
+
+    public function checkAuthorization($event){
+        if($event->user_id != Auth::user()->id){
+            return false;
+        }
+        return true;
+    }
+
+}
